f73eaab6ba49018ebf47c73338fd65fab6c04b49
Sergey <gershun62@gmail.com>
2024-01-06 12:10:25 +0200

    first task

--- js/script.js
+++ js/script.js
@@ -1,72 +1,43 @@
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
 'use strict';
 
-let number = 4.6;
-
-console.log(4/0);
-console.log('string' * 9);
-
-const obj = {
-	name: 'John',
-	age: 25,
-	isMarried: false
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
 };
 
-console.log(obj.name);
-// console.log(obj['name']); - правильно
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+	b = prompt('На сколько оцените его?', ''),
+	c = prompt('Один из последних просмотренных фильмов?', ''),
+	d = prompt('На сколько оцените его?', '');
 
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-console.log(arr[2]);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// alert('Hello');
-
-// const result = confirm('Are uou here?');
-// console.log('result');
-
-/* const answer = +prompt('Вам есть 18?', '18');
-console.log(answer + 5); */
-
-/* const answers = [];
-
-answers[0] = prompt('What is your name?', '');
-answers[1] = prompt('How many old are you?', '');
-answers[2] = prompt('Where are you from?', '');
-
-console.log(typeof(answers));
-console.log(typeof(null)); */
-
-/* const category = 'toys';
-
-console.log(`https://someurl.com/${category}/5`);
-
-const user = 'Sergey';
-
-alert(`Привет, ${user}`); */
-
-console.log('arr' + ' - object');
-console.log(4 + '5');
-
-let incr = 10,
-	decr = 10;
-
-incr++;
-decr--;
-
-console.log(incr);
-console.log(decr);
-
-console.log(5%2); /* Отобразить остаток */
-
-console.log(2*4 == 8); /* Сравнение */
-
-console.log(2*4 === '8'); /* Cтрогое сравнение числа и строки */
-
-// && - и, || - или
-
-const isChecked = true,
-	isClose = false;
-
-console.log(isChecked || isClose);
-
-/* ! - оператор отрицания */
-
-console.log(2 + 2 * 2 !== 8); /* 6 не равняется 8 */
+console.log(personalMovieDB);
